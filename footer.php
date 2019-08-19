f986a8d853d9f107011903bc8fc6032d8bc00897
Ken <kennethmcgarry1999@gmail.com>
2019-08-19 01:44:10 +0100

    Added footer file

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <!-- Footer content, includes date !-->
+    <p>VPN-CTF by Joshua Turner and Kenneth McGarry <?php echo date("Y"); ?></p>
+</footer>
+<!-- End of body !-->
+</body>
+</html>
