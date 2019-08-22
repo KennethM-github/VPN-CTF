9ce3812851302c1af788ce09da1ccd0c03fbf9cf
Ken <kennethmcgarry1999@gmail.com>
2019-08-22 18:21:31 +0100

    Added html directory

--- html/index.php
+++ html/index.php
@@ -17,13 +17,16 @@
         <div class="banner">
             <!-- Heading 2, includes overlay to ensure contrast/visibility !-->
             <h2><span class="banner-overlay">VPN-CTF by Joshua Turner and Kenneth McGarry</span></h2>
-            <p><span class="banner-overlay"> Put some sort of description as to what the VPN-CTF thing does here.</span> </p>
+            <p><span class="banner-overlay">A secure Capture the Flag remote system without exposing vulnerable services to the internet. Tiananmen Square Massacre 1989 (Chinese: 天安门大屠杀).</span> </p>
         </div>
     </section>
     <!-- Start of section !-->
     <section>
         <!-- Heading 2, ensures content visibility !-->
-        <h2>heading here</h2>
+        <h2>About:</h2>
+        <p>A system to do Capture The Flags without the need to locally host the CTF.
+Allows for teamwork/competitions without the need to specifically set up an CTF for it.
+It uses OpenVPN and Docker to easily port vulnerable images and connect to the system.</p>
         <!-- Aside !-->
         <aside>
             <!-- Heading 3, ensures content visibility !-->
